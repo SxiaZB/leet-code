4470287bf1ca4e20fd919939ee086431805685d7
zhuzhibo <zhuzhibo@yingmi.cn>
2020-05-29 08:51:32 +0800

    1106

--- src/main/java/com/leet/LeetCode.java
+++ src/main/java/com/leet/LeetCode.java
@@ -8,11 +8,57 @@ import java.util.function.IntConsumer;
 public class LeetCode {
 
     /**
+     * @link: https://leetcode-cn.com/problems/parsing-a-boolean-expression/
+     */
+    static class Solution1106 {
+        public boolean parseBoolExpr(String expression) {
+            return dfs(expression.toCharArray(), 0, '-') == 't';
+        }
+
+        private char dfs(char[] c, int i, char sign) {
+            char cur = '-';
+            while (i < c.length) {
+                char nowC = c[i];
+                if (nowC == '-' || nowC == ',' || nowC == '(') {
+                    i++;
+                    continue;
+                }
+                c[i] = '-';
+                if (nowC == ')') {
+                    break;
+                }
+                if (nowC == '!' || nowC == '|' || nowC == '&') {
+                    nowC = dfs(c, i + 1, nowC);
+                }
+                if (cur == '-') {
+                    cur = nowC;
+                }
+                switch (sign) {
+                    case '!':
+                        cur = !(nowC == 't') ? 't' : 'f';
+                        break;
+                    case '|':
+                        cur = ((cur == 't') || (nowC == 't')) ? 't' : 'f';
+                        break;
+                    case '&':
+                        cur = ((nowC == 't') && (cur == 't')) ? 't' : 'f';
+                        break;
+                    default:
+                        cur = nowC;
+                        break;
+                }
+                i++;
+            }
+            return cur;
+        }
+    }
+
+    /**
      * @link: https://leetcode-cn.com/problems/decode-string/
      */
     static class Solution394 {
         public String decodeString(String s) {
-            return dfs(s.toCharArray(),0);
+            return dfs(s.toCharArray(), 0);
         }
 
         private String dfs(char[] c, int i) {
